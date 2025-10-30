d6c9c5ca73c60dc9bbcd3b2eeaa4fffc1253f051
shahad <scientistphysics01@gmail.com>
2025-10-31 01:06:11 +0600

    Initial commit: derivative of f(x)

--- Prob-7-23.f95
+++ Prob-7-23.f95
@@ -1,37 +1,53 @@
-subroutine derivative(func, x_i, x_i_1, x_prime)
-    real, intent(in) :: x_i, x_i_1
-    real, intent(out) :: x_prime
-    real, external :: func 
+subroutine derivative(vector, deriv, nsamp, dx, error)
+    integer, intent(in) :: nsamp
+    real, intent(in) :: vector(nsamp)
+    real, intent(out) :: deriv(nsamp - 1)
+    real, intent(in) :: dx 
+    integer, intent(out) :: error
 
-    x_prime = (func(x_i_1) - func(x_i)) / (x_i_1 - x_i)   
+    integer :: i 
+
+    if (dx > 0.0) then 
+        do i = 1, nsamp - 1 
+            deriv(i) = ( vector(i + 1) - vector(i) ) / dx 
+        end do 
+
+        error = 0 
+    else 
+        error = 1
+    end if 
+    
 end subroutine derivative
 
-function f(x) result(res)
-    real :: res 
-    real, intent(in) :: x 
-
-    res = sin(x)
-
-end function f 
-
-program derivative_of_function
+program test_derivative
     implicit none
-    integer, parameter :: nsamp = 100
-    real :: start_val, step_size , ans
-    integer :: i 
-    real :: vec(nsamp)
-    real, external :: f 
-
-    start_val = 0. 
-    step_size = 0.05 
-
-    do i = 1, nsamp
-        vec(i) = start_val + i * step_size
-    end do 
     
-    call derivative(f, vec(1), vec(2), ans)
+    ! List of local constants
+    integer, parameter :: NSAMP = 100 
+    real, parameter :: DX = 0.05
 
-    print *, 'derivative = ', ans 
-    print *, cos(vec(1))
+    ! List of local variables
+    real, dimension(NSAMP-1) :: cderiv, deriv 
+    integer :: error, i 
+    real :: max_error 
+    real, dimension(NSAMP) :: vector 
 
-end program derivative_of_function
+    do i = 1, NSAMP 
+        vector(i) = sin(real(i-1) * DX)
+    end do 
+
+    ! Calculate analytic derivative of f(x)
+    do i = 1, NSAMP - 1
+        cderiv(i) = cos(real(i-1) * DX)
+    end do 
+
+    ! Call *derivative* 
+    call derivative(vector, deriv, NSAMP, DX, error)
+
+    ! Find the largest difference 
+    max_error = maxval(abs(deriv - cderiv))
+
+    ! Tell user 
+    write (*, 1000) max_error
+    1000 format(' The maximum error in the derivative is ', F10.4, '.')
+end program test_derivative
